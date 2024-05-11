2060d2e898e93d9d0e6673fb415ee60603b1ec8f
kishalaya1 <kishalaya1@yahoo.com>
2024-05-11 22:51:58 +0530

    unit test created

---
+++ TestWebsiteProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestWebsiteProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
